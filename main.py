5931b3f71850c1a8643952aba32df764f0eee380
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-02-11 15:47:28 -0800

    Google Maps Nearby Search integration
    https://github.com/googlemaps/google-maps-services-python/blob/9ec69cb66eec929d08ca90a82081b8ee4eef8b54/tests/test_places.py#L120

--- main.py
+++ main.py
@@ -5,17 +5,15 @@ import fastapi
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import StreamingResponse
 import googlemaps
-from geopy.distance import distance
 import pastforward
 
 static_dir = "static"
 static_dir_remote = f"/root/{static_dir}"
 gmp_api_key = "GMP_API_KEY"
 
-
 image = (
     modal.Image.debian_slim()
-    .pip_install("fastapi[standard]", "googlemaps", "geopy")
+    .pip_install("fastapi[standard]", "googlemaps")
     .add_local_python_source("pastforward")
     .add_local_dir(static_dir, remote_path=static_dir_remote)
 )
@@ -30,26 +28,24 @@ def api():
 
     @fastapi_app.get("/places")
     async def places(_request: fastapi.Request, coordinates: str):
-        # TODO: Replace reverse with nearby search. rank_by="distance",
-        # https://github.com/googlemaps/google-maps-services-python/blob/9ec69cb66eec929d08ca90a82081b8ee4eef8b54/tests/test_places.py#L120
+        gmp_places_nearby_results = gmaps.places_nearby(
+            location=parse_coordinates(coordinates),
+            type="point_of_interest",
+            language="en",
+            rank_by="distance",
+        )["results"]
+
+        # TODO: Filter places by historical significance.
         return reduce(
             lambda accumulator, current: (
                 accumulator
                 + (
                     []
                     if current["place_id"] is None
-                    or distance(
-                        parse_coordinates(coordinates),
-                        (
-                            current["geometry"]["location"]["lat"],
-                            current["geometry"]["location"]["lng"],
-                        ),
-                    ).km
-                    > 5
-                    else [pastforward.from_gmp_place(gmaps.place(current["place_id"]))]
+                    else [pastforward.from_gmp_place(current)]
                 )
             ),
-            gmaps.reverse_geocode(parse_coordinates(coordinates)),
+            gmp_places_nearby_results,
             [],
         )
 
--- pastforward.py
+++ pastforward.py
@@ -7,20 +7,22 @@ class PastforwardPlace(BaseModel):
     name: str
     latitude: float
     longitude: float
-    photo_refs: list[str] | None
+    types: list[str]
+    photo_refs: list[str]
 
 
-def from_gmp_place(details) -> PastforwardPlace:
+def from_gmp_place(gmp_place) -> PastforwardPlace:
     photo_refs = (
-        None
-        if not "photos" in details["result"]
-        else [x["photo_reference"] for x in details["result"]["photos"]]
+        []
+        if not "photos" in gmp_place
+        else [x["photo_reference"] for x in gmp_place["photos"]]
     )
     return PastforwardPlace(
-        id=details["result"]["place_id"],
-        address=details["result"]["formatted_address"],
-        name=details["result"]["name"],
-        latitude=details["result"]["geometry"]["location"]["lat"],
-        longitude=details["result"]["geometry"]["location"]["lng"],
+        id=gmp_place["place_id"],
+        address=gmp_place["vicinity"],
+        name=gmp_place["name"],
+        latitude=gmp_place["geometry"]["location"]["lat"],
+        longitude=gmp_place["geometry"]["location"]["lng"],
+        types=gmp_place["types"],
         photo_refs=photo_refs,
     )
