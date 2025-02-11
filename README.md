985dd3586c4dbff16cb90ef9e16ccd1537882823
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-02-10 19:36:18 -0800

    Google Maps: Format addresses from geocode

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Install the dependencies.
 
 ```sh
-python -m pip install modal
+python -m pip install modal googlemaps
 ```
 
 Set up the [Modal](https://modal.com/) project.
@@ -19,3 +19,7 @@ Run the development server. Live-reload might be supported on your system.
 ```sh
 python -m modal serve main.py
 ```
+
+Retrieve and store your environment variables:
+
+- [Google Maps Platform](https://console.cloud.google.com/google/maps-apis/onboard;flow=gmp-api-key-flow)
--- main.py
+++ main.py
@@ -1,25 +1,43 @@
+import os
 import modal
 
 static_dir = "static"
 static_dir_remote = f"/root/{static_dir}"
+gmp_api_key = "GMP_API_KEY"
 
-image = (modal.Image.debian_slim()
-            .pip_install("fastapi[standard]")
-            .add_local_dir(static_dir, remote_path=static_dir_remote))
+image = (
+    modal.Image.debian_slim()
+    .pip_install("fastapi[standard]", "googlemaps")
+    .add_local_dir(static_dir, remote_path=static_dir_remote)
+)
 app = modal.App(image=image)
 
-@app.function()
+
+@app.function(secrets=[modal.Secret.from_dotenv()])
 @modal.asgi_app()
 def api():
     import fastapi
     from fastapi.staticfiles import StaticFiles
+    import googlemaps
 
     fastapi_app = fastapi.FastAPI()
+    gmaps = googlemaps.Client(key=os.environ[gmp_api_key])
 
     @fastapi_app.get("/square/{x}")
-    async def square(_request: fastapi.Request, x: str):
-        return {"square": int(x)**2}
-        
+    async def square(_request: fastapi.Request, x: int):
+        return {"square": x**2}
+
+    @fastapi_app.get("/place")
+    async def place(_request: fastapi.Request, coordinates: str):
+        reverse_geocode_result = gmaps.reverse_geocode(parse_coordinates(coordinates))
+        formatted_addresses: list[str] = []
+        for x in reverse_geocode_result:
+            formatted_address = x["formatted_address"]
+            if formatted_address is None or formatted_address in formatted_addresses:
+                continue
+            formatted_addresses.append(x["formatted_address"])
+        return formatted_addresses
+
     fastapi_app.mount(
         "/",
         StaticFiles(directory=static_dir_remote, html=True),
@@ -27,3 +45,11 @@ def api():
     )
 
     return fastapi_app
+
+
+def parse_coordinates(s):
+    try:
+        lat, lon = map(float, s.replace(" ", "").split(","))
+        return lat, lon
+    except (ValueError, AttributeError):
+        return None
